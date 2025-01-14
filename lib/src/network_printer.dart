d0cd7022dba18c61a43a012cf48b9de0aeff8bb7
Nanan Setiady <infinia249@gmail.com>
2025-01-14 11:25:31 +0700

    [Fork] Exposed onError callback to investigate more details on failed connection

--- lib/src/network_printer.dart
+++ lib/src/network_printer.dart
@@ -2,57 +2,62 @@ import 'dart:io';
 import 'enums.dart';
 
 class PrinterNetworkManager {
-  late String _host;
-  int _port = 9100;
-  bool _isConnected = false;
-  Duration _timeout = const Duration(seconds: 5);
-  late Socket _socket;
 
-  PrinterNetworkManager(
-    String host, {
-    int port = 9100,
+  PrinterNetworkManager(this.host, {
+    this.port = 9100,
     Duration timeout = const Duration(seconds: 5),
-  }) {
-    _host = host;
-    _port = port;
-    _timeout = timeout;
-  }
+  }): _timeout = timeout;
 
-  Future<PosPrintResult> connect(
-      {Duration? timeout: const Duration(seconds: 5)}) async {
+  Future<PosPrintResult> connect({
+    Duration timeout = const Duration(seconds: 5,),
+    Function(Object error, StackTrace? stackTrace,)? onError,
+  }) async {
     try {
-      _socket = await Socket.connect(_host, _port, timeout: _timeout);
+      _socket = await Socket.connect(
+        host, port, timeout: _timeout,
+      );
       _isConnected = true;
       return Future<PosPrintResult>.value(PosPrintResult.success);
-    } catch (e) {
+    } catch (e, _) {
       _isConnected = false;
+      onError?.call(e, _,);
       return Future<PosPrintResult>.value(PosPrintResult.timeout);
     }
   }
 
-  Future<PosPrintResult> printTicket(List<int> data,
-      {bool isDisconnect = true}) async {
+  Future<PosPrintResult> printTicket(List<int> data, {
+    bool isDisconnect = true,
+    Function(Object error, StackTrace? stackTrace,)? onError,
+  }) async {
     try {
       if (!_isConnected) {
         await connect();
       }
-      _socket.add(data);
+      _socket?.add(data);
       if (isDisconnect) {
         await disconnect();
       }
       return Future<PosPrintResult>.value(PosPrintResult.success);
-    } catch (e) {
+    } catch (e, _) {
+      onError?.call(e, _,);
       return Future<PosPrintResult>.value(PosPrintResult.timeout);
     }
   }
 
-  Future<PosPrintResult> disconnect({Duration? timeout}) async {
-    await _socket.flush();
-    await _socket.close();
+  Future<PosPrintResult> disconnect({Duration? timeout,}) async {
+    await _socket?.flush();
+    await _socket?.close();
     _isConnected = false;
     if (timeout != null) {
       await Future.delayed(timeout, () => null);
     }
     return Future<PosPrintResult>.value(PosPrintResult.success);
   }
+
+  final String host;
+  final int port;
+  final Duration _timeout;
+
+  bool _isConnected = false;
+  Socket? _socket;
 }
